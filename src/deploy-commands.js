ff588c4c44abab1a0f84823a414cb99bbc0077ce
Sophia Beluli <sophiabeluli@gmail.com>
2024-11-07 00:10:31 -0500

    lots of fixes: adding/deleting event, adding/deleting role, assigning role all works; creator currently cannot get their role assigned

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -15,14 +15,24 @@ const rest = new REST().setToken(process.env.TOKEN);
 // and deploy your commands!
 (async () => {
     try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
+        );
 
         // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
-            body: commands,
-        });
+        const data = await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            {
+                body: commands,
+            }
+        );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
     } catch (error) {
         // And of course, make sure you catch and log any errors!
         console.error(error);
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,32 @@ import {
     GuildScheduledEvent,
     GuildScheduledEventStatus,
     PartialGuildScheduledEvent,
+    User,
 } from "discord.js";
 import "dotenv/config";
 import CustomClient from "./CustomClient";
 import allCommands from "./commands";
+import fs from "node:fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fileName = "saved.json";
+const file = __dirname + "/" + fileName;
 const token = process.env.TOKEN;
+let eventsRoles = new Map<string, string>();
+
+const saveFile = () => {
+    console.log("saving to file: ");
+    console.log(eventsRoles);
+    console.log(JSON.stringify(Object.fromEntries(eventsRoles)));
+    fs.writeFileSync(
+        file,
+        JSON.stringify(Object.fromEntries(eventsRoles)),
+        "utf8"
+    );
+};
 
 // Create a new client instance
 const client = new CustomClient({
@@ -33,9 +53,9 @@ client.on(Events.InteractionCreate, async (interaction) => {
         return;
     }
 
-    const command = (
-        interaction.client as CustomClient
-    ).commands.get(interaction.commandName);
+    const command = (interaction.client as CustomClient).commands.get(
+        interaction.commandName
+    );
     if (!command) {
         console.error(
             `No command matching ${interaction.commandName} was found.`
@@ -49,14 +69,12 @@ client.on(Events.InteractionCreate, async (interaction) => {
         console.error(err);
         if (interaction.replied || interaction.deferred) {
             await interaction.followUp({
-                content:
-                    "There was an error while executing this command!",
+                content: "There was an error while executing this command!",
                 ephemeral: true,
             });
         } else {
             await interaction.reply({
-                content:
-                    "There was an error while executing this command!",
+                content: "There was an error while executing this command!",
                 ephemeral: true,
             });
         }
@@ -67,9 +85,31 @@ client.on(Events.InteractionCreate, async (interaction) => {
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
-    console.log(
-        `Ready! Logged in as ${readyClient.user.tag}`
-    );
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+    // Load Data
+    if (!fs.existsSync(file)) {
+        console.log("file doesnt exist");
+        const content = JSON.stringify({});
+        fs.writeFileSync(file, content, "utf8");
+    }
+    let eventRolesString = JSON.parse(fs.readFileSync(file, "utf8"));
+    eventsRoles = new Map(Object.entries(eventRolesString));
+
+    // update roles TODO: so much work
+    // readyClient.guilds.fetch().then((guilds) => {
+    //     guilds.forEach(async (guild) => {
+    //         const guildObj = await guild.fetch();
+    //         const events = await guildObj.scheduledEvents.fetch();
+    //         events.forEach(async (event) => {
+    //             event.fetchSubscribers().then((subscribers) => {
+    //                 subscribers.forEach((subscriber) => {
+
+    //                 })
+    //             });
+    //         })
+    //     })
+    // })
 });
 
 // Log in to Discord with your client's token
@@ -80,11 +120,51 @@ client.on(
     async (
         guildScheduledEvent: GuildScheduledEvent<GuildScheduledEventStatus>
     ): Promise<void> => {
-        let subscribers =
-            await guildScheduledEvent.fetchSubscribers();
-        console.log(subscribers);
-        // const interestedUsers = res;
-        // console.log(interestedUsers);
+        console.log("event created");
+        guildScheduledEvent.guild.roles
+            .create({
+                name: guildScheduledEvent.name,
+                reason: "for event",
+            })
+            .then((role) => {
+                console.log("role created: " + role.id);
+                eventsRoles.set(guildScheduledEvent.id, role.id);
+                // add role to creator
+                console.log("event: " + guildScheduledEvent);
+                console.log("event id: " + guildScheduledEvent.id);
+                console.log("event creator: " + guildScheduledEvent.creator);
+                console.log("event status: " + guildScheduledEvent.status);
+                // guildScheduledEvent.guild.members.addRole({
+                //     role: role,
+                //     user: guildScheduledEvent.creator,
+                // });
+                // console.log("hi added role to creator");
+                saveFile();
+            })
+            .catch(console.error);
+    }
+);
+
+client.on(
+    Events.GuildScheduledEventDelete,
+    async (
+        guildScheduledEvent:
+            | GuildScheduledEvent<GuildScheduledEventStatus>
+            | PartialGuildScheduledEvent
+    ): Promise<void> => {
+        console.log("event deleted");
+        if (eventsRoles.get(guildScheduledEvent.id)) {
+            guildScheduledEvent.guild.roles
+                .delete(eventsRoles.get(guildScheduledEvent.id) || "")
+                .then(() => {
+                    console.log(
+                        "role deleted: " +
+                            eventsRoles.get(guildScheduledEvent.id)
+                    );
+                    eventsRoles.delete(guildScheduledEvent.id);
+                    saveFile();
+                });
+        }
     }
 );
 
@@ -93,14 +173,67 @@ client.on(
     async (
         _oldGuildScheduledEvent:
             | GuildScheduledEvent<GuildScheduledEventStatus>
-            | PartialGuildScheduledEvent
-            | null,
-        newGuildScheduledEvent: GuildScheduledEvent<GuildScheduledEventStatus>
+            | PartialGuildScheduledEvent,
+        newGuildScheduledEvent: GuildScheduledEvent
     ): Promise<void> => {
-        let subscribers =
-            await newGuildScheduledEvent.fetchSubscribers();
-        console.log(subscribers);
-        // const interestedUsers = res;
-        // console.log(interestedUsers);
+        if (
+            (newGuildScheduledEvent.status === 3 ||
+                newGuildScheduledEvent.status === 4) &&
+            eventsRoles.get(newGuildScheduledEvent.id)
+        ) {
+            console.log("event deleted");
+            // complete or canceled
+            newGuildScheduledEvent.guild.roles
+                .delete(eventsRoles.get(newGuildScheduledEvent.id) || "")
+                .then(() => {
+                    console.log(
+                        "role deleted: " +
+                            eventsRoles.get(newGuildScheduledEvent.id)
+                    );
+                    eventsRoles.delete(newGuildScheduledEvent.id);
+                    saveFile();
+                });
+        }
+    }
+);
+
+client.on(
+    Events.GuildScheduledEventUserAdd,
+    (
+        guildScheduledEvent:
+            | GuildScheduledEvent<GuildScheduledEventStatus>
+            | PartialGuildScheduledEvent,
+        user: User
+    ): void => {
+        console.log(
+            "user subscribing: " + user + " - " + guildScheduledEvent.id
+        );
+        console.log("role id: " + eventsRoles.get(guildScheduledEvent.id));
+        if (eventsRoles.get(guildScheduledEvent.id)) {
+            guildScheduledEvent.guild.members.addRole({
+                role: eventsRoles.get(guildScheduledEvent.id),
+                user: user,
+            });
+        }
+    }
+);
+
+client.on(
+    Events.GuildScheduledEventUserRemove,
+    (
+        guildScheduledEvent:
+            | GuildScheduledEvent<GuildScheduledEventStatus>
+            | PartialGuildScheduledEvent,
+        user: User
+    ): void => {
+        console.log(
+            "user unsubscribed: " + user + " - " + guildScheduledEvent.id
+        );
+        if (eventsRoles.get(guildScheduledEvent.id)) {
+            guildScheduledEvent.guild.members.removeRole({
+                role: eventsRoles.get(guildScheduledEvent.id),
+                user: user,
+            });
+        }
     }
 );
